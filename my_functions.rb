f8862e68e0c5d5c4af9bace1a581a1c5a5871790
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-20 16:34:03 +0000

    adds second function (sum_array)

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,12 @@
 def add_array_lengths(array1, array2)
   return array1.length() + array2.length()
 end
+
+
+def sum_array(array)
+  total = 0
+  for number in array
+    total += number
+  end
+  return total
+end
